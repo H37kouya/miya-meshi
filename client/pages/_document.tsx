5662bffd7bf874c6beb0dab8c957d63e94d1975e
H37kouya <h37kouya@gmail.com>
2020-07-23 21:49:58 +0900

    feat materialuiを入れた

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,23 +1,53 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components';
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const styledComponentSheets = new StyledComponentSheets();
+    const materialUiServerStyleSheets = new MaterialUiServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props =>
+            styledComponentSheets.collectStyles(
+              materialUiServerStyleSheets.collect(<App {...props} />)
+            )
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {styledComponentSheets.getStyleElement()}
+            {materialUiServerStyleSheets.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      styledComponentSheets.seal();
+    }
+  }
+
   render() {
     return (
       <Html lang="ja">
-        <Head>
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <link rel="icon" href="/favicon.ico" />
-          {/* <link rel="manifest" href="/manifest.json" /> */}
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
