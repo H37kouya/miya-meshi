05583bd8014a07fd1be4368910693902c09eb49e
H37kouya <h37kouya@gmail.com>
2020-08-05 17:46:32 +0900

    chore firebaseの設定追加

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -62,7 +62,7 @@ const nuxtConfig: Configuration = {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/device',
-    // '@nuxtjs/firebase',
+    '@nuxtjs/firebase',
     '@nuxtjs/pwa'
   ],
   /*
@@ -73,14 +73,19 @@ const nuxtConfig: Configuration = {
   firebase: {
     // options
     config: {
-      // apiKey: '<apiKey>',
-      // authDomain: '<authDomain>',
-      // databaseURL: '<databaseURL>',
-      // projectId: '<projectId>',
-      // storageBucket: '<storageBucket>',
-      // messagingSenderId: '<messagingSenderId>',
-      // appId: '<appId>',
-      // measurementId: '<measurementId>'
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    },
+    services: {
+      auth: true, // Just as example. Can be any other service.
+      analytics: true,
+      storage: true
     }
   },
   styleResources: {
