ec3fbc44cd3312966183dfd21207b48b320d1513
H37kouya <h37kouya@gmail.com>
2020-08-07 23:23:52 +0900

    feat indexのデザイン追加

--- client/pages/admin/shops/index.vue
+++ client/pages/admin/shops/index.vue
@@ -1,26 +1,53 @@
 <template>
   <v-container>
-    <h1>Shop ページ</h1>
+    <v-row justify="space-between">
+      <h1>店舗一覧</h1>
 
-    <h2>ログイン済み</h2>
+      <v-btn to="/admin/shops/create" color="success">
+        新規店舗追加
+      </v-btn>
+    </v-row>
 
-    <v-btn to="/admin/shops/create">
-      Create Shop
-    </v-btn>
+    <v-card>
+      <v-list two-line>
+        <template v-for="(shop, idx) in state.shops">
+          <v-list-item :key="shop.id" :to="`/admin/shops/${shop.id}`">
+            <v-list-item-avatar>
+              <v-icon
+                :color="shop.public ? 'success' : 'grey'"
+              >{{ shop.public ? 'mdi-check-circle' : 'mdi-alert-circle' }}</v-icon>
+            </v-list-item-avatar>
 
-    <v-list>
-      <template v-for="(shop, key) in state.shops">
-        <v-list-item :key="key" :to="`/admin/shops/${shop.id}`">
-          {{ shop.id }}: {{ shop.name }}
-        </v-list-item>
-      </template>
-    </v-list>
+            <v-list-item-content>
+              <v-list-item-title>{{ shop.name }}</v-list-item-title>
+              <v-list-item-subtitle v-if="shop.intro">{{ shop.intro }}</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn icon>
+                <v-icon v-if="1 <= shop.priority && shop.priority <= 5" color="grey">
+                  {{ `mdi-numeric-${shop.priority}-circle` }}
+                </v-icon>
+                <v-icon v-else color="grey">
+                  mdi-numeric-1-circle
+                </v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+
+          <v-divider
+            v-if="idx + 1 < state.shops.length"
+            :key="idx"
+          />
+        </template>
+      </v-list>
+    </v-card>
   </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, SetupContext, onMounted } from '@vue/composition-api'
-import {Shop, SHOP_TYPE} from '@/src/types/Shop'
+import { Shop, SHOP_TYPE } from '@/src/types/Shop'
 
 const getShopList = async (context: SetupContext) => {
   return await context.root.$fireStore.collection('shops').get()
@@ -31,7 +58,7 @@ const firestoreDocDataToShop = (doc: firebase.firestore.QueryDocumentSnapshot<fi
   return {
     type: SHOP_TYPE,
     id: doc.id,
-    name: docData.name
+    ...docData
   } as Shop
 }
 
