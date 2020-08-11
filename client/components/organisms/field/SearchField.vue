2a1bd4406a6007fda8fec6cb13ae1c21f420ff76
H37kouya <h37kouya@gmail.com>
2020-08-11 12:02:58 +0900

    feat 検索ボタンの作成

--- client/components/organisms/field/SearchField.vue
+++ client/components/organisms/field/SearchField.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-container>
+  <v-container class="max-width-350">
+    <div class="mb-2">
+      <SearchTextField />
+    </div>
+
     <v-row justify="center">
       <ToSearchShopButton class="mr-2" />
 
