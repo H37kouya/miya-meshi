cd2d78de29b2c5293f98b8fe0b8ad4b13e700f49
H37kouya <h37kouya@gmail.com>
2020-08-11 12:15:19 +0900

    feat キーワードから探すのボタン作成

--- client/components/organisms/field/SearchField.vue
+++ client/components/organisms/field/SearchField.vue
@@ -1,13 +1,31 @@
 <template>
-  <v-container class="max-width-350">
-    <div class="mb-2">
-      <SearchTextField />
-    </div>
+  <div style="background-color: #f5f5f5;">
+    <v-container class="max-width-350">
+      <div class="mb-2">
+        <SearchTextField />
+      </div>
 
-    <v-row justify="center">
-      <ToSearchShopButton class="mr-2" />
+      <div class="mb-2">
+        <v-btn outlined width="100%" small class="justify-end">
+          <span class="search-keyword">
+            キーワードから探す
+          </span>
+        </v-btn>
+      </div>
 
-      <ToSearchFoodButton />
-    </v-row>
-  </v-container>
+      <v-row justify="center">
+        <ToSearchShopButton class="mr-2" />
+
+        <ToSearchFoodButton />
+      </v-row>
+    </v-container>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.search-keyword {
+  &::after {
+    content: '▼';
+  }
+}
+</style>
