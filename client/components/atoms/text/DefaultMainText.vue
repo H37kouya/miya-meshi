adafb4d35817dd9dba8990f8b6f65334f6fd1689
H37kouya <h37kouya@gmail.com>
2020-08-11 16:42:44 +0900

    feat メニューの表示

---
+++ client/components/atoms/text/DefaultMainText.vue
@@ -0,0 +1,36 @@
+<template>
+  <component
+    :is="`h${level}`"
+    class="default-main-text u-black--text text-center"
+  >
+    <slot />
+  </component>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+type Props = {
+  level: 1|2|3|4|5|6
+}
+
+export default defineComponent({
+  props: {
+    level: {
+      type: Number,
+      default: 1,
+      validator (v?: number) {
+        return v ? [1, 2, 3, 4, 5, 6].includes(v) : false
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.default-main-text {
+  font-size: 1.25rem;
+  font-weight: bolder;
+  padding: 0.5rem;
+}
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,97 +1,56 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div>
+    <DefaultMainText>
+      今週のおすすめ
+    </DefaultMainText>
+
+    <v-container>
+      <v-row justify="center">
+        <template v-for="(menu, key) in list">
+          <v-col :key="key" cols="4" md="3" class="px-1">
+            <MenuCard v-bind="menu" />
+          </v-col>
+        </template>
+      </v-row>
+    </v-container>
+
+    <DefaultMainText>
+      新着情報
+    </DefaultMainText>
+  </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+<script lang="ts">
+import { computed, defineComponent, SetupContext } from '@vue/composition-api'
 
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
+export default defineComponent({
+  setup (_, context: SetupContext) {
+    const list = computed(() => {
+      return [
+        {
+          name: 'ハンバーグ&チキン',
+          price: 976,
+          src: '/sample_niku.jpg',
+          isTaxIncluded: true
+        },
+        {
+          name: '三種のチーズ牛丼',
+          price: 1080,
+          src: '/sample_niku.jpg',
+          isTaxIncluded: false
+        },
+        {
+          name: 'とろける4種チーズのフォルマッジ',
+          price: 976,
+          src: '/sample_niku.jpg',
+          isTaxIncluded: true
+        }
+      ]
+    })
+
+    return {
+      list
+    }
   }
-}
+})
 </script>
