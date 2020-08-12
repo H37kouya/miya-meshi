ea8db932e5e305473b7ada2ab9c6ed0cb0eed891
H37kouya <h37kouya@gmail.com>
2020-08-12 22:52:08 +0900

    feat 木目の背景を追加

--- client/components/layouts/TheDefaultFooter.vue
+++ client/components/layouts/TheDefaultFooter.vue
@@ -5,8 +5,9 @@
     class="d-block pa-0"
   >
     <div class="recruitment d-flex align-center justify-center">
-      <a href="" class="white--text text-decoration-none">
-        このサイトに掲載希望の飲食店さんはこちら！
+      <a href="" class="white--text text-decoration-none w-100 pos-relative">
+        <span class="recruitment-text">このサイトに掲載希望の飲食店さんはこちら！</span>
+        <v-img src="/tree-background.png" width="100%" max-height="90px" />
       </a>
     </div>
 
@@ -50,5 +51,13 @@
   text-align: center;
   height: 90px;
   font-size: 1rem;
+}
+
+.recruitment-text {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 5;
 }
 </style>
