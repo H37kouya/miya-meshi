14dd3744e3e4d181b89dbe2f39ff2467485f3a21
H37kouya <h37kouya@gmail.com>
2020-08-13 22:53:19 +0900

    広告の追加

---
+++ client/components/atoms/ad_field/FiveOneAdField.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="pos-relative">
+    <component :is="href ? `a` : 'div'" :href="href" target="_blank" rel="noopener">
+      <v-img v-bind="$attrs" aspect-ratio="5" />
+    </component>
+
+    <div class="pr-field red--text">
+      <span class="pr">PR</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    href: {
+      type: String,
+      default: undefined
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.pr-field {
+  position: absolute;
+  top: 0;
+  left: 0.5rem;
+}
+
+.pr {
+  &::before {
+    content: '[';
+  }
+
+  &::after {
+    content: ']';
+  }
+}
+</style>
