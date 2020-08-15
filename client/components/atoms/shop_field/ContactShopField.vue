e6ebe33784a716b1316bb4ad68fbde70946ee139
H37kouya <h37kouya@gmail.com>
2020-08-15 23:59:27 +0900

    refactor ファイル分けをした

--- client/components/atoms/shop_field/ContactShopField.vue
+++ client/components/atoms/shop_field/ContactShopField.vue
@@ -1,81 +1,24 @@
 <template>
-  <table class="contact-table u-black--text">
-    <template v-for="(data, key) in tableData">
-      <tr :key="key">
-        <th class="table-heading text-center">
-          {{ data.heading }}
-        </th>
-        <td class="table-value text-center">
-          {{ data.value }}
-        </td>
-      </tr>
-    </template>
-  </table>
+  <div>
+    <DefaultMainText :level="2">
+      お問い合わせ
+    </DefaultMainText>
+
+    <div class="u-light-grey-background mb-4">
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12" class="d-flex justify-center">
+            <ContactShopField :shop="state.shop" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, reactive, SetupContext, watch } from '@vue/composition-api'
-import { Shop, ShopJa } from '@/src/types/Shop'
-
-type TableData = {
-  heading: string,
-  value: string|number
-}
-const convertToTableData = (shop: Shop): TableData[] => {
-  const tableData = [] as TableData[]
-
-  if (shop.address) {
-    const value = shop.buildingName ? `${shop.address}${shop.buildingName}` : shop.address
-    tableData.push({
-      heading: ShopJa.ADDRESS,
-      value
-    } as TableData)
-  }
-
-  if (shop.tel) {
-    tableData.push({
-      heading: ShopJa.TEL,
-      value: shop.tel
-    } as TableData)
-  }
-
-  if (shop.businessHour1 || shop.businessHour2) {
-    if (shop.businessHour1 && shop.businessHour2) {
-      tableData.push({
-        heading: ShopJa.BUSINESS_HOUR,
-        value: `${shop.businessHour1}<br>${shop.businessHour2}`
-      })
-    } else {
-      tableData.push({
-        heading: ShopJa.BUSINESS_HOUR,
-        value: `${shop.businessHour1}`
-      })
-    }
-  }
-
-  if (shop.parkingLot) {
-    tableData.push({
-      heading: ShopJa.PARKING_LOT,
-      value: shop.parkingLot
-    })
-  }
-
-  if (shop.regularHoliday) {
-    tableData.push({
-      heading: ShopJa.REGULAR_HOLIDAY,
-      value: shop.regularHoliday
-    })
-  }
-
-  if (shop.seat) {
-    tableData.push({
-      heading: ShopJa.SEAT,
-      value: shop.seat
-    })
-  }
-
-  return tableData
-}
+import { defineComponent, reactive, SetupContext, watch } from '@vue/composition-api'
+import { Shop } from '@/src/types/Shop'
 
 type Props = {
   shop: Shop
@@ -94,54 +37,13 @@ export default defineComponent({
       shop: {} as Shop
     })
 
-    const tableData = computed(() => convertToTableData(state.shop))
-
     watch(() => props.shop, (newVal, _) => {
       state.shop = newVal
     })
 
     return {
-      tableData,
       state
     }
   }
 })
 </script>
-
-<style lang="scss" scoped>
-.table-value {
-  padding: 1rem 0.5rem;
-  width: 220px;
-  font-size: 0.9rem;
-  border: {
-    right: 1px #d2d2d2 solid;
-    bottom: 1px #d2d2d2 solid;
-  };
-}
-
-.table-heading {
-  background-color: #fdf4d2;
-  padding: 1rem 0.5rem;
-  width: 80px;
-  font-size: 0.9rem;
-  border: {
-    left: 1px #d2d2d2 solid;
-    right: 1px #d2d2d2 solid;
-    bottom: 1px #d2d2d2 solid;
-  }
-}
-
-.contact-table {
-  border-spacing: 0;
-
-  tr:first-of-type {
-    .table-heading {
-      border-top: 1px #d2d2d2 solid;
-    }
-
-    .table-value {
-      border-top: 1px #d2d2d2 solid;
-    }
-  }
-}
-</style>
---
+++ client/components/atoms/shop_field/RecommendShopField.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <DefaultMainText :level="2">
+      当店のおすすめ
+    </DefaultMainText>
+
+    <div class="u-light-grey-background">
+      <v-container class="max-width-600 py-4">
+        <template v-for="(menu, key) in state.menus">
+          <div :key="menu.id" class="px-4" :class="{ 'mb-4': state.menus.length - 1 !== key }">
+            <ShopRecommendListItem :menu="menu" />
+          </div>
+        </template>
+      </v-container>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, SetupContext, watch } from '@vue/composition-api'
+import { Menu } from '@/src/types/Menu'
+
+type Props = {
+  menus: Menu[]
+}
+
+export default defineComponent({
+  props: {
+    menus: Object,
+    default: () => []
+  },
+
+  setup (props: Props, context: SetupContext) {
+    const state = reactive({
+      menus: [] as Menu[]
+    })
+
+    watch(() => props.menus, (newVal, _) => {
+      state.menus = newVal
+    })
+
+    return {
+      state
+    }
+  }
+})
+</script>
