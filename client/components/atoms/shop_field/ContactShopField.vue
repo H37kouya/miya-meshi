029236cbc8308e335bae2d81b22a34294ef06a48
H37kouya <h37kouya@gmail.com>
2020-08-16 09:38:09 +0900

    feat snsのアイコンの追加

--- client/components/atoms/shop_field/ContactShopField.vue
+++ client/components/atoms/shop_field/ContactShopField.vue
@@ -11,14 +11,57 @@
             <ContactShopTable :shop="state.shop" />
           </v-col>
         </v-row>
+
+        <v-row justify="center" class="mt-4">
+          <template v-for="(snsLink, key) in snsLinks">
+            <v-btn
+              :key="key"
+              :href="snsLink.href"
+              target="_blank"
+              rel="noopener"
+              icon
+              x-large
+              class="mx-2"
+            >
+              <v-img :src="snsLink.src" :alt="snsLink.name" aspect-ratio="1" />
+            </v-btn>
+          </template>
+        </v-row>
       </v-container>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, SetupContext, watch } from '@vue/composition-api'
+import { computed, defineComponent, reactive, SetupContext, watch } from '@vue/composition-api'
 import { Shop } from '@/src/types/Shop'
+
+type SnsLink = {
+  name: string,
+  href: string,
+  src: string
+}
+
+enum snsImage {
+  facebook = '/facebook.png',
+  github = '/github.png',
+  hp = '/hp.png',
+  instagram = '/instagram.png',
+  line = '/line.png',
+  twitter = '/twitter.png',
+  youtube = '/youtube.png'
+}
+
+const shopToSnsLinks = (shop: Shop): SnsLink[] => {
+  const snsLinks = [] as SnsLink[]
+  shop.instaLink && snsLinks.push({ name: 'instagram', href: shop.instaLink, src: snsImage.instagram })
+  shop.twitterLink && snsLinks.push({ name: 'twitter', href: shop.twitterLink, src: snsImage.twitter })
+  shop.homepageLink && snsLinks.push({ name: 'homepage', href: shop.homepageLink, src: snsImage.hp })
+  shop.facebookLink && snsLinks.push({ name: 'facebook', href: shop.facebookLink, src: snsImage.facebook })
+  shop.lineLink && snsLinks.push({ name: 'line', href: shop.lineLink, src: snsImage.line })
+  shop.youtubeLink && snsLinks.push({ name: 'youtube', href: shop.youtubeLink, src: snsImage.youtube })
+  return snsLinks
+}
 
 type Props = {
   shop: Shop
@@ -37,11 +80,14 @@ export default defineComponent({
       shop: {} as Shop
     })
 
+    const snsLinks = computed(() => shopToSnsLinks(state.shop))
+
     watch(() => props.shop, (newVal, _) => {
       state.shop = newVal
     })
 
     return {
+      snsLinks,
       state
     }
   }
