73bf98f6307e5edabdf466b91dfc91f4506ecc75
H37kouya <h37kouya@gmail.com>
2020-08-18 16:28:26 +0900

    feat headerにMediumサイズを追加

--- client/components/layouts/TheDefaultHeader.vue
+++ client/components/layouts/TheDefaultHeader.vue
@@ -2,24 +2,31 @@
   <header>
     <HeaderLongField v-if="isLong" />
 
+    <HeaderMediumField v-else-if="isMedium" />
+
     <HeaderShortField v-else />
   </header>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, SetupContext } from '@vue/composition-api'
+const LONG_ROUTES = [
+  '/'
+]
+
+const MEDIUM_ROUTES = [
+  '/shops',
+  '/menus'
+]
 
 export default defineComponent({
   setup (_, context: SetupContext) {
-    const longRoutes = [
-      '/',
-      '/shops',
-      '/menus'
-    ]
-    const isLong = computed(() => longRoutes.includes(context.root.$route.path))
+    const isLong = computed(() => LONG_ROUTES.includes(context.root.$route.path))
+    const isMedium = computed(() => MEDIUM_ROUTES.includes(context.root.$route.path))
 
     return {
-      isLong
+      isLong,
+      isMedium
     }
   }
 })
---
+++ client/components/organisms/field/HeaderMediumField.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="w-100">
+      <div class="top-image" />
+      <div class="text-right px-4 developer-name">
+        <small class="developer-name-small">運営: みやメシ応援隊 開発: U-lab</small>
+      </div>
+    </div>
+
+    <div>
+      <SearchField />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "heder_field";
+
+.top-image {
+  background-color: grey;
+  width: 100%;
+  height: 120px;
+}
+</style>
