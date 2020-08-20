3cbaf37132d8ca6a12e8e482671fd44d924701c3
H37kouya <h37kouya@gmail.com>
2020-08-20 16:42:06 +0900

    feat アスペクト比をつけた

--- client/components/organisms/field/HeaderMediumField.vue
+++ client/components/organisms/field/HeaderMediumField.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="w-100">
-      <v-img src="/miyameshi-header.jpg" width="100%" height="160px" />
+      <v-img src="/miyameshi-header.jpg" width="100%" max-height="160px" aspect-ratio="5.77" />
       <div class="text-right px-4 developer-name">
         <small class="developer-name-small">運営: みやメシ応援隊 開発: U-lab</small>
       </div>
