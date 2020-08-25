6d5ce48262d9712401d0ecf249838f555743b4ca
H37kouya <h37kouya@gmail.com>
2020-08-25 09:56:11 +0900

    feat 配列のテストを追加

--- client/src/utils/Array.ts
+++ client/src/utils/Array.ts
@@ -11,5 +11,5 @@ export const isArray = (v: any): v is any[] => Array.isArray(v)
  * @param { any } v
  */
 export const isNullArray = (v: any): v is null[] => {
-  return isArray(v) && v.every((c: any) => c === null)
+  return isArray(v) && v.length > 0 && v.every((c: any) => c === null)
 }
