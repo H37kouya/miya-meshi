b5a830efd8b851061661ab58863c56e65a8c1aff
H37kouya <h37kouya@gmail.com>
2020-08-26 22:46:53 +0900

    少し修正

--- admin/jest.config.js
+++ admin/jest.config.js
@@ -2,17 +2,28 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js',
+    '^vue$': 'vue/dist/vue.common.js'
   },
-  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'vue',
+    'json'
+  ],
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    '.*\\.(vue)$': 'vue-jest'
   },
+  setupFilesAfterEnv: [
+    '<rootDir>/test/setup.ts'
+  ],
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
+    '<rootDir>/middleware/**/*.vue',
     '<rootDir>/pages/**/*.vue',
-  ],
+    '<rootDir>/store/**/*.ts',
+    '<rootDir>/src/**/*.ts'
+  ]
 }
--- client/src/CompositonFunctions/utils/UseCounter.ts
+++
@@ -1,7 +0,0 @@
-export const useCounter = (str?: string, maxLength: number = 50, magnification: number = 0.8) => {
-  const isVisible = !!str && str.length > maxLength * magnification
-
-  const counter = isVisible ? maxLength : undefined
-
-  return { counter }
-}
