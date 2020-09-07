84e3e0765421ad52464196d1df00821ea4571683
H37kouya <h37kouya@gmail.com>
2020-09-07 17:42:26 +0900

    client側にlang/jaを追加

---
+++ client/lang/ja/Menu.ts
@@ -0,0 +1,9 @@
+export enum Menu {
+  NAME            = 'メニュー名',
+  DESCRIPTION     = '一言紹介',
+  INTRO           = 'メニュー紹介',
+  PRICE           = '値段',
+  PRIORITY        = '優先度',
+  IS_TAX_INCLUDED = '税込みかどうか',
+  CAN_TAKEOUT     = 'テイクアウトできるかどうか'
+}
