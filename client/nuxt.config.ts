a1e37210d5cbcdf60e0be50a48d2c8c63af07643
H37kouya <h37kouya@gmail.com>
2020-09-13 17:11:16 +0900

    fix types

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -90,14 +90,14 @@ const nuxtConfig: NuxtConfig = {
   firebase: {
     // options
     config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+      apiKey: process.env.FIREBASE_API_KEY || '',
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
+      databaseURL: process.env.FIREBASE_DATABASE_URL || '',
+      projectId: process.env.FIREBASE_PROJECT_ID || '',
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
+      appId: process.env.FIREBASE_APP_ID || '',
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID || ''
     },
     services: {
       firestore: true,
