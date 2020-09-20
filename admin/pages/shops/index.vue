f174b103036442ae347ef1c9fc6243910bd9e112
H37kouya <h37kouya@gmail.com>
2020-09-20 15:39:45 +0900

    feat 管理者画面のshopsをpaginationにした

--- admin/pages/shops/index.vue
+++ admin/pages/shops/index.vue
@@ -12,23 +12,57 @@
 
     <v-row>
       <v-col cols="12">
-        <AdminShopListCard :shops="state.shops" />
+        <div class="py-4">
+          <v-form @submit.prevent="onSearch">
+            <div class="d-flex align-center max-width-350">
+              <v-text-field
+                v-model="state.searchText"
+                class="mr-4"
+                outlined
+                hide-details
+                label="店舗名で検索"
+              />
+
+              <v-btn type="submit" color="primary">
+                検索
+              </v-btn>
+            </div>
+          </v-form>
+        </div>
+
+        <AdminShopListCard :shops="pagination.shops" />
+
+        <div v-if="pagination.totalVisible !== 1" class="pt-4">
+          <v-pagination
+            :value="nowPage"
+            :length="pagination.totalVisible"
+            @input="onPagination"
+          />
+        </div>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, SetupContext, onMounted } from '@vue/composition-api'
+import { defineComponent, reactive, SetupContext, onMounted, computed } from '@vue/composition-api'
 import { MetaInfo } from 'vue-meta'
 import { Shop } from '@/lib'
 import { getShopList } from '@/src/infra/firestore/Shop'
+import { isArray } from '@/src/utils/Array'
+import { isString } from '~/src/utils/String'
+
+type State = {
+  searchText: string,
+  shops: Shop[]
+}
 
 export default defineComponent({
   middleware: 'admin-auth',
 
   setup (_: unknown, context: SetupContext) {
-    const state = reactive({
+    const state = reactive<State>({
+      searchText: isString(context.root.$route.query.name) ? context.root.$route.query.name : '',
       shops: [] as Shop[]
     })
 
@@ -36,8 +70,62 @@ export default defineComponent({
       state.shops = await getShopList(context.root.$fireStore, 0, true)
     })
 
+    const filterShops = computed(() => {
+      const name = context.root.$route.query.name
+      if (name) {
+        return state.shops.filter((shop: Shop) => {
+          if (!shop.name) {
+            return false
+          }
+          return name.includes(shop.name)
+        })
+      }
+
+      return state.shops
+    })
+
+    const pagination = computed(() => {
+      return {
+        shops: filterShops.value.slice((nowPage.value - 1) * 10, nowPage.value * 10),
+        totalVisible: Math.floor(filterShops.value.length / 10) + 1
+      }
+    })
+
+    const nowPage = computed(() => {
+      const page = context.root.$route.query.page
+      if (isArray(page)) {
+        return 1
+      }
+
+      const pageNumber = Number(page)
+      return Number.isInteger(pageNumber) ? pageNumber : 1
+    })
+
+    const onPagination = async (page: number) => {
+      return await context.root.$router.push({
+        path: '/shops',
+        query: {
+          page: String(page)
+        }
+      })
+    }
+
+    const onSearch = async () => {
+      return await context.root.$router.push({
+        path: '/shops',
+        query: {
+          name: state.searchText || undefined
+        }
+      })
+    }
+
     return {
-      state
+      filterShops,
+      state,
+      nowPage,
+      pagination,
+      onPagination,
+      onSearch
     }
   },
 
