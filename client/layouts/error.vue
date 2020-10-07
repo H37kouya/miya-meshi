d3d1e38f629080c22583fe38db51c186547c5257
H37kouya <h37kouya@gmail.com>
2020-10-07 22:09:15 +0900

    feat 画像の差し替えといらない画像の削除

--- client/layouts/error.vue
+++ client/layouts/error.vue
@@ -4,28 +4,90 @@
       <v-breadcrumbs :items="breadcrumbs" class="py-0 px-0 px-sm-6" />
     </v-container>
 
-    <v-container class="px-0">
-      <MainText v-if="error.statusCode === 404">
-        {{ error.statusCode === 404 ? pageNotFound : otherError }}
-      </MainText>
+    <v-container class="py-0">
+      <v-row>
+        <v-col cols="12" class="pa-0">
+          <MainText>
+            {{ error.statusCode === 404 ? pageNotFound : otherError }}
+          </MainText>
 
-      <h2 class="error-detail mb-2 pt-2 px-2 border-t border-dark-gray">
-        お探しのページが見つかりませんでした
-      </h2>
+          <h2 class="error-detail py-4 px-2 border-t border-dark-gray">
+            {{ explanation[error.statusCode === 404 ? 404 : 500] }}
+          </h2>
+        </v-col>
+      </v-row>
 
-      <div class="d-flex justify-center">
-        <div class="max-width-480">
-          <v-img
-            src="/s/error-not-found.jpg"
-            alt="お探しのページが見つかりませんでした"
-          />
-        </div>
+      <div class="py-sm-8">
+        <template v-if="error.statusCode === 404">
+          <div class="d-sm-none">
+            <v-card
+              max-width="360px"
+              width="100%"
+              flat
+              class="mx-auto"
+            >
+              <v-img
+                :src="Image404Mobile"
+                :alt="explanation[404]"
+              />
+            </v-card>
+          </div>
+
+          <div class="d-none d-sm-block">
+            <v-card
+              class="mx-auto"
+              flat
+              max-width="800px"
+              width="100%"
+            >
+              <v-img
+                :src="Image404Pc"
+                :alt="explanation[404]"
+              />
+            </v-card>
+          </div>
+        </template>
+
+        <template v-else>
+          <div class="d-sm-none">
+            <v-card
+              max-width="360px"
+              width="100%"
+              flat
+              class="mx-auto"
+            >
+              <v-img
+                :src="Image500Mobile"
+                :alt="explanation[500]"
+              />
+            </v-card>
+          </div>
+
+          <div class="d-none d-sm-block">
+            <v-card
+              class="mx-auto"
+              flat
+              max-width="800px"
+              width="100%"
+            >
+              <v-img
+                :src="Image500Pc"
+                :alt="explanation[500]"
+              />
+            </v-card>
+          </div>
+        </template>
       </div>
     </v-container>
   </div>
 </template>
 
 <script>
+import Image404Mobile from '@/assets/image/miyameshi404_mobile.jpg'
+import Image404Pc from '@/assets/image/miyameshi404_pc.jpg'
+import Image500Mobile from '@/assets/image/miyameshi500_mobile.jpg'
+import Image500Pc from '@/assets/image/miyameshi500_pc.jpg'
+
 const breadcrumbs = [
   { exact: true, text: 'Home', to: '/' },
   { exact: true, text: 'お店で探す', to: '/shops' }
@@ -41,12 +103,22 @@ export default {
   },
   data () {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      pageNotFound: 'Page Not Found',
+      otherError: 'Internal Server Error'
     }
   },
   computed: {
-    breadcrumbs: () => breadcrumbs
+    breadcrumbs: () => breadcrumbs,
+
+    explanation: () => ({
+      404: '404 - お探しのページが見つかりませんでした',
+      500: '500 - サーバーでエラーが発生しました。'
+    }),
+
+    Image404Mobile: () => Image404Mobile,
+    Image404Pc: () => Image404Pc,
+    Image500Mobile: () => Image500Mobile,
+    Image500Pc: () => Image500Pc
   },
 
   head () {
