e5ebd7ed9a28dab6d2ea96bac92b329b687a6882
H37kouya <h37kouya@gmail.com>
2020-10-16 11:22:08 +0900

    feat created_at, updated_atの追加

--- backend/app/Repositories/CreateSelectionPostAreaRepository.php
+++ backend/app/Repositories/CreateSelectionPostAreaRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use Exception;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
@@ -22,10 +23,13 @@ class CreateSelectionPostAreaRepository
     ): array {
         try {
             DB::beginTransaction();
+            $now = Carbon::now();
 
-            $data = (new Collection($firebaseAreaIds))->map(fn($firebaseAreaId) => [
+            $data = (new Collection($firebaseAreaIds))->map(fn(string $firebaseAreaId) => [
                 'selection_post_id' => $selectionPostId,
                 'firebase_area_id'  => $firebaseAreaId,
+                'created_at'        => $now,
+                'updated_at'        => $now,
             ])->all();
 
             DB::table('selection_post_areas')->insert($data);
