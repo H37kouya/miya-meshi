028b5575faddc1aa0e99833150a16dd7611f9c4d
H37kouya <h37kouya@gmail.com>
2020-10-16 22:19:53 +0900

    fix propsの追加

--- client/components/atoms/shop_field/RecommendShopField.vue
+++ client/components/atoms/shop_field/RecommendShopField.vue
@@ -5,7 +5,7 @@
         <template v-for="(menu, key) in menus">
           <v-col :key="menu.id" cols="12" md="4" class="px-4 py-0 py-md-3" :class="{ 'mb-4': menus.length - 1 !== key }">
             <template v-if="screenMd">
-              <MenuCard v-bind="menu" :src="menu.image" />
+              <MenuCard v-bind="menu" :menu="menu" :src="menu.image" />
             </template>
 
             <template v-else>
