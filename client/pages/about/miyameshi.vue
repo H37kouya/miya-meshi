801eecef8cd0e776564ddc74de60046c4e6db7cc
H37kouya <h37kouya@gmail.com>
2020-11-25 16:15:34 +0900

    fix

--- client/pages/about/miyameshi.vue
+++ client/pages/about/miyameshi.vue
@@ -88,7 +88,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, SetupContext, useMeta } from '@nuxtjs/composition-api'
+import { defineComponent, useMeta } from '@nuxtjs/composition-api'
 import { useNews } from '@/src/CompositonFunctions/news/UseNews'
 import { Breadcrumb } from '@/lib'
 
