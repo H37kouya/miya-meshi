2639757fea79d105a9d3cc6aa27ad72b2c9360b5
H37kouya <h37kouya@gmail.com>
2020-12-04 20:19:28 +0900

    feat 管理者画面に日付設定の追加

--- admin/lib/types/Post.ts
+++ admin/lib/types/Post.ts
@@ -5,6 +5,8 @@ export type Post = {
   contents: string
   image: string
   release: boolean
+  publish_from: string|null
+  publish_to: string|null
   firebase_area_ids: string[]
   firebase_shop_ids: string[]
   created_at: string
