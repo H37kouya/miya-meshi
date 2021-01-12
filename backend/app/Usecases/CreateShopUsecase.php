c83dcce61dc9487412eb51a15f5b65405321c252
Toshiaki <conamn1185@gmail.com>
2021-01-12 17:24:10 +0900

    PeriodOfTime周りでクラスを2つ消した

--- backend/app/Usecases/CreateShopUsecase.php
+++ backend/app/Usecases/CreateShopUsecase.php
@@ -43,39 +43,3 @@ class CreateShopUsecase
         }
     }
 }
-class RegisterPeriodOfTime
-{
-    private CreateShopRepository $_createShopRepository;
-
-    /**
-     * Create a new usecase instance.
-     *
-     * @return void
-     */
-    public function __construct(CreateShopRepository $createShopRepository)
-    {
-        $this->_createShopRepository = $createShopRepository;
-    }
-
-    /**
-     * invoke
-     *
-     * @return void
-     */
-    public function invoke(
-        array $inputs
-    ) {
-        DB::beginTransaction();
-
-        try {
-            $shop = $this->_createShopRepository->invoke($inputs);
-            DB::commit();
-            Cache::flush();
-
-            return $shop;
-        } catch (Exception $e) {
-            DB::rollBack();
-            throw $e;
-        }
-    }
-}
