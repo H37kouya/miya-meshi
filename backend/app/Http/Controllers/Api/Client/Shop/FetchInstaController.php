9e6c452b136db61b357f1e2ac8c3d7f42079dd3d
Toshiaki <conamn1185@gmail.com>
2021-01-21 14:44:16 +0900

    insta取得を修正

--- backend/app/Http/Controllers/Api/Client/Shop/FetchInstaController.php
+++ backend/app/Http/Controllers/Api/Client/Shop/FetchInstaController.php
@@ -7,17 +7,19 @@ use App\Models\Shop;
 use Request;
 use Illuminate\Support\Carbon;
 
-
 class FetchInstaController extends Controller
 {
     public function __invoke(Request $request)
     {
         $shops = Shop::with([
-            "shopInformation", "images"
+            "shopInformation"
+            => function ($query) {
+                $query->orderBy("insta_number", "desc"); //instaNumberに関して降順で取得する
+            }, "images"
             => function ($query) {
                 $query->where("imageable_name", "image_link");
             }
-        ])->nowPublicPosts(Carbon::now())->limit(10)->orderBy('instaNumber', 'desc')->get(); //現在公開中のshopを10件まで取得する
+        ])->nowPublicPosts(Carbon::now())->limit(10)->get(); //現在公開中のshopを10件まで取得する
         return $shops;
     } //withはリレーション先を読み込む
 }
