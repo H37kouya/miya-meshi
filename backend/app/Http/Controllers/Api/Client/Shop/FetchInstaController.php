66725f43b894405ed78e6ee497ce179bb1b415c0
H37kouya <h37kouya@gmail.com>
2021-01-23 23:19:46 +0900

    feat orderbynの修正

--- backend/app/Http/Controllers/Api/Client/Shop/FetchInstaController.php
+++ backend/app/Http/Controllers/Api/Client/Shop/FetchInstaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Client\Shop;
 
+use App\Enum\Models\ShopModel;
 use App\Http\Controllers\Controller;
 use App\Models\Shop;
 use Illuminate\Http\Request;
@@ -14,15 +15,26 @@ class FetchInstaController extends Controller
     {
         $limit = $request->query('limit', '10');
         $shops = Shop::with([
-            "shopInformation", 
+            "shopInformation",
             "images"
             => function ($query) {
                 $query->where("imageable_name", "image_link")->get();
             }
-        ])->nowPublicPosts(Carbon::now())
-        ->join('shop_information', 'shop_information.shop_id', '=', 'shops.id')
-        ->orderBY('shop_information.insta_number', 'desc')
-        ->limit($limit)->get(); //現在公開中のshopを$requestで指定された件数まで取得する
+        ])
+            ->nowPublicPosts(Carbon::now())
+            ->select(
+                'shops.'.ShopModel::id,
+                'shops.'.ShopModel::release,
+                'shops.'.ShopModel::publish_from,
+                'shops.'.ShopModel::publish_to,
+                'shops.'.ShopModel::priority,
+                'shops.'.ShopModel::display_mode,
+                'shops.'.ShopModel::created_at,
+                'shops.'.ShopModel::updated_at,
+            )
+            ->join('shop_information', 'shop_information.shop_id', '=', 'shops.id')
+            ->orderBY('shop_information.insta_number', 'desc')
+            ->limit($limit)->get(); //現在公開中のshopを$requestで指定された件数まで取得する
         return $shops;
     } //withはリレーション先を読み込む
 }
