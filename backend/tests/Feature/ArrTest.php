4945a74a8d6692159170ea25606518ae7c8e908d
H37kouya <h37kouya@gmail.com>
2021-02-26 21:23:53 +0900

    fix coding style

--- backend/tests/Feature/ArrTest.php
+++ backend/tests/Feature/ArrTest.php
@@ -15,25 +15,26 @@ class ArrTest extends TestCase
      * @return void
      */
 
-    private const test_arrs_snake = [
+    private $test_arrs_snake = [
         [1, 2, 3, 4, 5, 6],
         ["one_one" => 1, "two_two" => 2, "three_three" => 3, "four_four" => 4, "five_five" => 5, "six_six" => 6],
     ];
-    private const proper_arrs_snake = [
+    private $proper_arrs_snake = [
         [1, 2, 3, 4, 5, 6],
         ["oneOne" => 1, "twoTwo" => 2, "threeThree" => 3, "fourFour" => 4, "fiveFive" => 5, "sixSix" => 6],
     ];
-    private const test_arrs_camel = [
+    private $test_arrs_camel = [
         [1, 2, 3, 4, 5, 6],
         ["oneOne" => 1, "twoTwo" => 2, "threeThree" => 3, "fourFour" => 4, "fiveFive" => 5, "sixSix" => 6],
     ];
-    private const proper_arrs_camel = [
+    private $proper_arrs_camel = [
         [1, 2, 3, 4, 5, 6],
         ["one_one" => 1, "two_two" => 2, "three_three" => 3, "four_four" => 4, "five_five" => 5, "six_six" => 6],
     ];
-    public static function testArr()
+
+    public function testArr()
     {
-        foreach (self::test_arrs_snake as $test_arr) {
+        foreach ($this->test_arrs_snake as $test_arr) {
             $cameled[] = Arr::camel_keys($test_arr); //テストすべき配列のキーをキャメル化する
         }
         foreach ($cameled as $arr) {
@@ -41,13 +42,13 @@ class ArrTest extends TestCase
             $j = 0;
             $keys = array_keys($arr); //配列をひとつづつキーだけ取り出す
             foreach ($keys as $value) {
-                self::assertEquals($value, array_keys(self::proper_arrs_snake[$i])[$j]);
+                $this->assertEquals($value, array_keys($this->proper_arrs_snake[$i])[$j]);
                 $j++;
             }
             $i++;
         } //ここまでがcamel_keysのテスト
         // ここからsnake_keysのテスト
-        foreach (self::test_arrs_camel as $test_arr) {
+        foreach ($this->test_arrs_camel as $test_arr) {
             $snaked[] = Arr::snake_keys($test_arr); //テストすべき配列のキーをスネーク化する
         }
         foreach ($snaked as $arr) {
@@ -55,7 +56,7 @@ class ArrTest extends TestCase
             $l = 0;
             $keys = array_keys($arr); //配列をひとつづつキーだけ取り出す
             foreach ($keys as $value) {
-                self::assertEquals($value, array_keys(self::proper_arrs_camel[$k])[$l]);
+                $this->assertEquals($value, array_keys($this->proper_arrs_camel[$k])[$l]);
                 $l++;
             }
             $k++;
