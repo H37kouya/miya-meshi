5d72f9424fb92ad76870bdba86efb067a7dd166f
toshiaki <conamn1185@gmail.com>
2021-03-08 11:52:59 +0900

    feat testUpdateShopUsecase

---
+++ backend/tests/Feature/testUpdateShopUsecase.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Image;
+use App\Models\Shop;
+use App\Models\ShopInformation;
+use App\Usecases\CreateShopUsecase;
+use App\Usecases\UpdateShopUsecase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class testUpdateShopUsecase extends TestCase
+{
+    use RefreshDatabase;
+    private $updateShopAndShopInformationList = [];
+    private $updateImageList = [];
+    /**
+     * test on UpdateShopUsecase.php
+     *
+     * @return void
+     */
+
+    public function testUpdateShopUsecase(
+        UpdateShopUsecase $_UpdateShopUsecase,
+        CreateShopUsecase $_CreateShopUsecase,
+    ) {
+        // set
+        $shopInstance = factory(Shop::class)->create();
+        $shopId = $shopInstance->id;
+        $shopAndShopInformationInsertInstance = $shopInstance->shopInformation->save(factory(ShopInformation::class))->make();
+        $imageInsertInstance = factory(Image::class)->create();
+        $shopAndShopInformationUpdateInstance = $shopAndShopInformationInsertInstance->create($this->updateShopAndShopInformationList);
+        $imageUpdateInstance = $imageInsertInstance->create($this->updateImageList);
+        // exec
+        $_CreateShopUsecase->invoke(array_merge($shopAndShopInformationInsertInstance->toArray(), $imageInsertInstance->toArray()));
+        $_UpdateShopUsecase->invoke((int)$shopId, array_merge($shopAndShopInformationUpdateInstance->toArray(), $imageUpdateInstance->toArray()));
+        // comparison
+        $this->assertTrue(Shop::where("id", $shopAndShopInformationInstance->id)->exists());
+        $this->assertTrue(ShopInformation::where("id", $shopAndShopInformationInstance->shopInformation->id)->exists());
+        $this->assertTrue(Image::where("id", $imageId)->exists());
+    }
+}
