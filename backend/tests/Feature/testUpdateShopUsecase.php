f9bfad89479dc3f5449126f6511e07d2d44aa695
toshiaki <conamn1185@gmail.com>
2021-03-10 23:07:36 +0900

    テストそのものが実行できない

--- backend/tests/Feature/testUpdateShopUsecase.php
+++ backend/tests/Feature/testUpdateShopUsecase.php
@@ -30,15 +30,20 @@ class testUpdateShopUsecase extends TestCase
         $shopInstance = factory(Shop::class)->create();
         $shopId = $shopInstance->id;
         $shopAndShopInformationInsertInstance = $shopInstance->shopInformation->save(factory(ShopInformation::class))->make();
+        $shopInstance = null;
+
         $imageInsertInstance = factory(Image::class)->create();
+        $imageId = $imageInsertInstance->id;
+
         $shopAndShopInformationUpdateInstance = $shopAndShopInformationInsertInstance->create($this->updateShopAndShopInformationList);
         $imageUpdateInstance = $imageInsertInstance->create($this->updateImageList);
+
         // exec
         $_CreateShopUsecase->invoke(array_merge($shopAndShopInformationInsertInstance->toArray(), $imageInsertInstance->toArray()));
         $_UpdateShopUsecase->invoke((int)$shopId, array_merge($shopAndShopInformationUpdateInstance->toArray(), $imageUpdateInstance->toArray()));
         // comparison
-        $this->assertTrue(Shop::where("id", $shopAndShopInformationInstance->id)->exists());
-        $this->assertTrue(ShopInformation::where("id", $shopAndShopInformationInstance->shopInformation->id)->exists());
+        $this->assertTrue(Shop::where("id", $shopAndShopInformationUpdateInstance->id)->exists());
+        $this->assertTrue(ShopInformation::where("id", $shopAndShopInformationUpdateInstance->shopInformation->id)->exists());
         $this->assertTrue(Image::where("id", $imageId)->exists());
     }
 }
